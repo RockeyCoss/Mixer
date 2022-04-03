9b21b91ddd19f71e6e67e9335800011b576ed3cc
RockeyCoss <1350291943@qq.com>
2022-04-04 07:24:31 +0800

    add v3, 4, 5

---
+++ mix_adapter/configs/upernet/upernet_vit-mix-adapterv5_8x2_512x512_160k_ade20k.py
@@ -0,0 +1,3 @@
+_base_ = 'upernet_vit-mix-adapterv1_8x2_512x512_160k_ade20k.py'
+
+model = dict(backbone=dict(type='ViTMixAdapterv5'))
--- mix_adapter/models/backbone/__init__.py
+++ mix_adapter/models/backbone/__init__.py
@@ -1,4 +1,8 @@
 from .vit_mix_adapterv1 import ViTMixAdapterv1
-from .vit_mix_adpaterv2 import ViTMixAdapterv2
+from .vit_mix_adapterv2 import ViTMixAdapterv2
+from .vit_mix_adapterv3 import ViTMixAdapterv3
+from .vit_mix_adapterv4 import ViTMixAdapterv4
+from .vit_mix_adapterv5 import ViTMixAdapterv5
 
-__all__ = ['ViTMixAdapterv1', 'ViTMixAdapterv2']
+__all__ = ['ViTMixAdapterv1', 'ViTMixAdapterv2', 'ViTMixAdapterv3',
+           'ViTMixAdapterv4', 'ViTMixAdapterv5']
