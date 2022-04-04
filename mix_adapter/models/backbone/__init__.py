22c536921fa975a23a8c34250b0f88699a7afcac
RockeyCoss <1350291943@qq.com>
2022-04-04 19:43:07 +0800

    add v7

--- mix_adapter/models/backbone/__init__.py
+++ mix_adapter/models/backbone/__init__.py
@@ -4,6 +4,8 @@ from .vit_mix_adapterv3 import ViTMixAdapterv3
 from .vit_mix_adapterv4 import ViTMixAdapterv4
 from .vit_mix_adapterv5 import ViTMixAdapterv5
 from .vit_mix_adapterv6 import ViTMixAdapterv6
+from .vit_mix_adapterv7 import ViTMixAdapterv7
 
 __all__ = ['ViTMixAdapterv1', 'ViTMixAdapterv2', 'ViTMixAdapterv3',
-           'ViTMixAdapterv4', 'ViTMixAdapterv5', 'ViTMixAdapterv6']
+           'ViTMixAdapterv4', 'ViTMixAdapterv5', 'ViTMixAdapterv6',
+           'ViTMixAdapterv7']
