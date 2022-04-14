e99055003b721173af1bccec7aa778c7d1ed57f7
RockeyCoss <1350291943@qq.com>
2022-04-15 04:30:01 +0800

    modify

--- mix_adapter/configs/upernet/upernet_vit-mix-adapterv23_8x2_512x512_160k_ade20k.py
+++ mix_adapter/configs/upernet/upernet_vit-mix-adapterv23_8x2_512x512_160k_ade20k.py
@@ -1,3 +1,6 @@
 _base_ = 'upernet_vit-mix-adapterv1_8x2_512x512_160k_ade20k.py'
 
 model = dict(backbone=dict(type='ViTMixAdapterv23'))
+data = dict(
+    # By default, models are trained on 4 GPUs with 4 images per GPU
+    samples_per_gpu=4)
--- mix_adapter/configs/upernet/upernet_vit-mix-adapterv24_8x2_512x512_160k_ade20k.py
+++ mix_adapter/configs/upernet/upernet_vit-mix-adapterv24_8x2_512x512_160k_ade20k.py
@@ -1,3 +1,7 @@
 _base_ = 'upernet_vit-mix-adapterv1_8x2_512x512_160k_ade20k.py'
 
 model = dict(backbone=dict(type='ViTMixAdapterv24'))
+
+data = dict(
+    # By default, models are trained on 4 GPUs with 4 images per GPU
+    samples_per_gpu=4)
